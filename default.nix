0b09b22245c0773c0626e7d807a978fcbbae156c
marble <git@computer-in.love>
2024-01-23 12:03:18 +0100

    download home-manager depending on version

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib, ... }:
+let
+  nixpkgs_version = "${lib.versions.major lib.version}.${lib.versions.minor lib.version}";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-${nixpkgs_version}.tar.gz";
+in
 {
   imports = [
     # requires `sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager` to work
-    <home-manager/nixos>
+    # <home-manager/nixos>
+    (import "${home-manager}/nixos")
     ./desktop
     ./users
     ./audio.nix
